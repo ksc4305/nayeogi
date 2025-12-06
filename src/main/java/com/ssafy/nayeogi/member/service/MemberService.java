316fdf3eaa53230f92cbe81f8b5d4efc66a0146e
Seonghwan Park <qtg9651@naver.com>
2025-12-07 00:23:57 +0900

    Feat: 로그인, 로그아웃 API 구현

--- src/main/java/com/ssafy/nayeogi/member/service/MemberService.java
+++ src/main/java/com/ssafy/nayeogi/member/service/MemberService.java
@@ -14,7 +14,7 @@ public interface MemberService {
 	 * @param memberDto 가입할 회원 정보
 	 * @throws Exception 아이디 중복 시 예외 발생
 	 */
-	void join(MemberDto memberDto) throws Exception;
+	void join(MemberDto memberDto) ;
 	
 	/**
 	 * 아이디 중복 체크
@@ -22,6 +22,14 @@ public interface MemberService {
 	 * @return 해당 아이디의 개수 (0 또는 1)
 	 * @throws Exception 예외
 	 */
-	int idCheck(String userId) throws Exception;
+	int idCheck(String userId) ;
+	
+	/**
+	 * 로그인 처리
+	 * @param userId 입력받은 아이디
+	 * @param userPassword 입력받은 비밀번호
+	 * @return 로그인 성공 시 회원정보(MemberDto), 실패 시 null
+	 */
+	MemberDto login(String userId, String userPassword);
 	
 }
