acdd8ab4a10fea8754ed80fd9503bbe2dd7a71df
kangsancheon <rkdtkscjs1@naver.com>
2025-12-09 19:51:22 +0900

    작업 내용 저장

--- src/main/java/com/ssafy/nayeogi/common/exception/ErrorCode.java
+++ src/main/java/com/ssafy/nayeogi/common/exception/ErrorCode.java
@@ -12,6 +12,7 @@ public enum ErrorCode {
     // 1. 공통 에러
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
     SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다."),
+    FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
 
     // 2. 회원 관련 (MEMBER_)
     MEMBER_ID_DUPLICATE(HttpStatus.BAD_REQUEST, "이미 사용 중인 아이디입니다."),
@@ -20,9 +21,12 @@ public enum ErrorCode {
 
     // 3. 이미지/파일 관련 (FILE_)
     FILE_SIZE_EXCEEDED(HttpStatus.BAD_REQUEST, "파일 용량이 10MB를 초과했습니다."),
-    FILE_UPLOAD_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다.");
-
-    private final HttpStatus httpStatus;
+    FILE_UPLOAD_FAIL(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다."), 
+    
+    // 4. 스토리 관련 (STORY_)
+    STORY_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 스토리입니다.");
+    
+private final HttpStatus httpStatus;
     private final String message;
     
 }
--- src/main/java/com/ssafy/nayeogi/story/model/dto/StorySaveRequest.java
+++ src/main/java/com/ssafy/nayeogi/story/model/dto/StorySaveRequest.java
@@ -3,25 +3,31 @@ package com.ssafy.nayeogi.story.model.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.List;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Data
 @NoArgsConstructor
+@Schema(description = "스토리북 저장 요청 DTO")
 public class StorySaveRequest {
     
+	@Schema(hidden = true) // DB에서 생성되므로 입력받지 않음
     private int id;
+    
+    @Schema(description = "여행 계획 ID", example = "101")
     private int planId;
+    
+    @Schema(description = "스토리북 제목", example = "나의 부산 여행 (감성 Ver)")
     private String title;
+    
+    @Schema(description = "대표 이미지 URL", example = "https://s3.../cover.jpg")
     private String thumbnailPath;
+    
+    @Schema(description = "공개 여부", example = "true")
     private boolean isPublic;
+    
+    @Schema(hidden = true) // 토큰에서 추출하므로 입력받지 않음?
     private String memberId;
     
-    private List<StoryPageRequest> pages;
-
-    @Data
-    @NoArgsConstructor
-    public static class StoryPageRequest {
-        private int contentId;
-        private String imagePath;
-        private String content;
-    }
+    @Schema(description = "스토리 페이지 목록")
+    private List<StoryPageDto> pages;
 }
