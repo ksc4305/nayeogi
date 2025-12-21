697af9048784fed7c2714acf0d129622b36ee4f2
kangsancheon <rkdtkscjs1@naver.com>
2025-12-22 01:02:47 +0900

    ai 설정

--- src/main/java/com/ssafy/nayeogi/NayeogiApplication.java
+++ src/main/java/com/ssafy/nayeogi/NayeogiApplication.java
@@ -1,9 +1,10 @@
 package com.ssafy.nayeogi;
 
+import org.springframework.ai.model.google.genai.autoconfigure.chat.GoogleGenAiChatAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = GoogleGenAiChatAutoConfiguration.class)
 public class NayeogiApplication {
 
 	public static void main(String[] args) {
